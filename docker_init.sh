f38f61443c498317b72730e5678d1decb6bb7644
Zvonimir Tomesic <t.zvonimir@gmail.com>
2017-10-24 14:46:03 +0200

    Docker changes

--- docker_init.sh
+++ docker_init.sh
@@ -2,10 +2,13 @@
 
 service nginx start
 
+cd /core
+npm install
 cd /core/src/admin-api
 composer install
-php artisan migrate:refresh --seed
 cd /core/src/admin-vue
 npm install
 npm install -g quasar-cli
+cd /core/src/admin-api
+php artisan migrate:refresh --seed
 tail -f /dev/null
