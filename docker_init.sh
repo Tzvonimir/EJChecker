d76ce71061d20cf2cfeac6a03ce12377ea6be9c5
Zvonimir Tomesic <t.zvonimir@gmail.com>
2018-09-10 19:08:55 +0200

    Fix docker file

--- docker_init.sh
+++ docker_init.sh
@@ -8,7 +8,7 @@ cd /core/src/admin-api
 composer install
 cd /core/src/admin-vue
 npm install
-npm install -g quasar-cli
+npm install -g quasar-cli@0.6.5
 cd /core/src/admin-api
 php artisan migrate:refresh --seed
 tail -f /dev/null
